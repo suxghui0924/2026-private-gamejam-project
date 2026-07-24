f9f7e0e5ccd8b70d060784dd3b878a83c6ca758d
Caramel_appple <papersion1210@gmail.com>
2026-07-24 22:06:06 +0900

    LoadingScene

---
+++ Assets/_Scripts/LSO/UIScripts/LSO_ModuleSelect.cs
@@ -0,0 +1,10 @@
+using _Scripts.Suxghui.Manager;
+using _Scripts.Suxghui.Mining;
+using UnityEngine;
+
+public class LSO_ModuleSelect : MonoBehaviour
+{
+    public MiningTechType miningTechType;
+    
+    //private string Tech = GameManager.Instance.T
+}
--- Assets/_Scripts/LSO/UIScripts/LSO_StartGame.cs
+++ Assets/_Scripts/LSO/UIScripts/LSO_StartGame.cs
@@ -4,10 +4,10 @@ using UnityEngine.SceneManagement;
 public class LSO_StartGame : MonoBehaviour
 {
     [Header("넘어 가는 씬")]
-    [SerializeField] private int sceneIndex = 1;
+    [SerializeField] private string startScene = "StarField";
     
     public void StartGame()
     {
-        SceneManager.LoadScene(sceneIndex);
+        LoadingSceneController.LoadScene(startScene);
     }
 }
